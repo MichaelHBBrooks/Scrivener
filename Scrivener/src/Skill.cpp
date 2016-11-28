379b6b915f682fad7fdd1ebccd12d511015aeaa6
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2016-11-27 23:59:08 -0500

    Skill documentation added.

--- Scrivener/src/Skill.cpp
+++ Scrivener/src/Skill.cpp
@@ -8,30 +8,60 @@
 #include "Skill.h"
 #include <string>
 
+/**
+ * Sets a skill value to 0, but assigns no name.
+ */
 Skill::Skill(){
 	value = 0;
 }
 
+/**
+ * A new skill is assigned a name and value.
+ * @param newName
+ * @param newValue
+ */
 Skill::Skill(std::string& newName, short newValue){
 	name = newName;
 	value = newValue;
 }
 
+/**
+ * A new skill is assigned a name while the value defaults to 0.
+ * @param newName
+ */
 Skill::Skill(std::string& newName){
 	name = newName;
 	value = 0;
 }
 
+/**
+ * Sets the skill value to newValue.
+ * @param newValue
+ */
 void Skill::setSkillValue(short newValue){
 	value = newValue;
 }
+
+/**
+ * Returns the skill value.
+ * @return
+ */
 short Skill::getSkillValue(){
 	return value;
 }
 
+/**
+ * Sets the skill name to newName.
+ * @param newName
+ */
 void Skill::setSkillName(std::string newName){
 	name = newName;
 }
+
+/**
+ * Returns the skill name.
+ * @return
+ */
 std::string Skill::getSkillName(){
 	return name;
 }
