54b4255167a8fffd95abcb64e2cd597a92182ab2
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2016-12-11 00:00:45 -0500

    Attribute documentation.

--- Scrivener/src/character/Attribute.cpp
+++ Scrivener/src/character/Attribute.cpp
@@ -12,33 +12,61 @@ Attribute::Attribute(){
 	value = 10;
 }
 
+/**
+ * Create a new attribute with a predefined value.
+ * @param newAttribute
+ */
 Attribute::Attribute(int newAttribute){
 	value = newAttribute;
 }
 
 
+/**
+ * Set the value of the attribute.
+ * @param newAttribute
+ */
 void Attribute::setValue(int newAttribute){
 	value = newAttribute;
 }
 
+/**
+ * Return the value of the attribute.
+ * @return
+ */
 int Attribute::getValue(){
 	return value;
 }
 
 
+/**
+ * Set the name of the attribute.
+ * @param newName
+ */
 void Attribute::setName(std::string newName){
 	name = newName;
 }
 
+/**
+ * Return the value of the attribute.
+ * @return
+ */
 std::string Attribute::getName(){
 	return name;
 }
 
 
+/**
+ * Set the abbreviated name of the attribute.
+ * @param newAbbreviation
+ */
 void Attribute::setAbbreviation(std::string newAbbreviation){
 	abbreviation = newAbbreviation;
 }
 
+/**
+ * Return the abbreviated attribute name.
+ * @return
+ */
 std::string Attribute::getAbbreviation(){
 	return abbreviation;
 }
